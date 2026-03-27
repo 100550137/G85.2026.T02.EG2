447f499b355deb6e941dc08b2d97cbceba7b30d7
100550137 <100550137@alumnos.uc3m.es>
2026-03-27 01:01:47 +0100

    cambios del validate_cif por los casos de prueba

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -117,28 +117,41 @@ class EnterpriseManager:
 
     @staticmethod
     def validate_cif(cif: str):
-        """Validate CIF code."""
-        letter = cif[0]
-        control_caracter = ["J","A","B","C","D","E","F","G","H","I"]
-        number = cif[1:8]
-        add_even = int(number[1]) + int(number[3]) + int(number[5])
-        add_odd = 0
-        i = 0
-        while i <= 6:
-            odd = str(int(number[i]) * 2)
-            if len(odd) == 2:
-                odd = int(odd[0]) + int(odd[1])
-            add_odd += int(odd)
-            i += 2
-        total_sum = str(add_even + add_odd)
-        if total_sum[1] == "0":
-            digit = 0
-        else:
-            digit = str(10 - int(total_sum[1]))
-        if letter in ("A", "B", "E", "H") and (cif[8] != digit):
-            return False
-        if letter in ("K", "P", "Q", "S"):
-            digit = control_caracter[int(digit)]
-            if cif[8] != digit:
+        """Algoritmo de validación de CIF blindado contra caracteres no numéricos"""
+        try:
+            cif = cif.upper()
+            if len(cif) != 9:
                 return False
-        return True
+
+            letter = cif[0]
+            number = cif[1:8]
+            control = cif[8]
+
+            # Comprobar que la parte central son TODO números
+            if not number.isdigit():
+                return False
+
+            # Suma posiciones pares
+            add_even = int(number[1]) + int(number[3]) + int(number[5])
+
+            # Suma posiciones impares
+            add_odd = 0
+            for i in range(0, 7, 2):
+                res = int(number[i]) * 2
+                add_odd += (res // 10) + (res % 10)
+
+            total_sum = add_even + add_odd
+            last_digit = int(str(total_sum)[-1])
+            digit_control = (10 - last_digit) % 10
+
+            letter_control = "JABCDEFGHI"[digit_control]
+
+            if letter in "ABEH":
+                return control == str(digit_control)
+            elif letter in "KPQS":
+                return control == letter_control
+            else:
+                return control == str(digit_control) or control == letter_control
+        except Exception:
+            # Cualquier error inesperado (letras donde no deben, etc.) devuelve False
+            return False
